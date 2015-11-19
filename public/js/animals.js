df9709b9aa073f8eb362a611726e09b7c738e613
jurporan <jan.purro@heig-vd.ch>
2015-11-19 14:56:11 +0100

    First version (only with charts)

---
+++ public/js/animals.js
@@ -0,0 +1,7 @@
+var tweb = angular.module('animals', ["chart.js"]);
+
+tweb.controller("barChart", function($scope)
+{
+    $scope.labels = ["Cats", "Dogs", "Horses", "Other"];
+    $scope.data	= [867, 257, 145, 187];
+});
